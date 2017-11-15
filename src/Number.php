6e3f3156400cd6cfa75a9dcacfb89eb294b81594
TahiriSiamak <tahirisiamak08@gmail.com>
2017-11-15 16:36:41 +0100

    Implemented Number

--- src/Number.php
+++ src/Number.php
@@ -7,10 +7,19 @@ namespace CaT\CLICalc;
  */
 class Number implements Expression {
 	/**
+	 * @var int
+	 */
+	private $number;
+
+	/**
 	 * @param	int	the number
 	 */
 	public function __construct($number) {
 		// TODO: fill me
+		if(!is_numeric($number)) {
+			throw new \InvalidArgumentException($number." is not nummeric");
+		}
+		$this->number = $number;
 	}
 
 	/**
@@ -18,5 +27,7 @@ class Number implements Expression {
 	 */
 	public function evaluate() {
 		// TODO: fill me
+		return $this->number;
 	}
+
 }
