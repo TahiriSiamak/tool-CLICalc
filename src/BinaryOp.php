27a6c9e4d36fd4a39e4f6f5a87611d9271ffa295
TahiriSiamak <tahirisiamak08@gmail.com>
2017-11-15 16:36:03 +0100

    Implemented BinaryOp

--- src/BinaryOp.php
+++ src/BinaryOp.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace CaT\CLICalc;
 
 /**
@@ -7,12 +6,30 @@ namespace CaT\CLICalc;
  */
 class BinaryOp implements Expression {
 	/**
+	 * @var string
+	 */
+	private $op;
+
+	/**
+	 * @var Expression
+	 */
+	private $left;
+
+	/**
+	 * @var Expression
+	 */
+	private $right;
+
+	/**
 	 * @param	string		$op
 	 * @param	Expression	$left
 	 * @param	Expression	$right
 	 */
-	public function __construct($op, Expression $left, Expression $right) {
+	public function __construct(string $op, Expression $left, Expression $right) {
 		// TODO: fill me
+		$this->op = $op;
+		$this->left = $left;
+		$this->right = $right;
 	}
 
 	/**
@@ -20,5 +37,21 @@ class BinaryOp implements Expression {
 	 */
 	public function evaluate() {
 		// TODO: fill me
+		if($this->op == '+')
+		{
+			return $this->left->evaluate() + $this->right->evaluate();
+		}
+		if($this->op == '-')
+		{
+			return $this->left->evaluate() - $this->right->evaluate();
+		}
+		if($this->op == '*')
+		{
+			return $this->left->evaluate() * $this->right->evaluate();
+		}
+		if($this->op == '/')
+		{
+			return $this->left->evaluate() / $this->right->evaluate();
+		}
 	}
 }
