5660dacbbcca3e65d341f07fd24722e4898ddd9f
TahiriSiamak <tahirisiamak08@gmail.com>
2017-11-15 16:38:02 +0100

    Implemented different kind of tests for binary implementations

--- tests/ExpressionTest.php
+++ tests/ExpressionTest.php
@@ -12,8 +12,9 @@ class ExpressionTest extends PHPUnit_Framework_TestCase {
 	} 
 
 	public function test_number_10() {
-		// TODO: fill me
-		$this->assertFalse(true);
+		// TODO: fill
+		$number = new Number(10);
+		$this->assertEquals(10, $number->evaluate());
 	}
 
 	public function test_plus() {
@@ -36,12 +37,14 @@ class ExpressionTest extends PHPUnit_Framework_TestCase {
 
 	public function test_subtraction() {
 		// TODO: fill me
-		$this->assertFalse(true);
+		$node = new BinaryOp("-", new Number(6), new Number(2));
+		$this->assertEquals(4, $node->evaluate());
 	}
 
 	public function test_division() {
 		// TODO: fill me
-		$this->assertFalse(true);
+		$node = new BinaryOp("/", new Number(6), new Number(2));
+		$this->assertEquals(3, $node->evaluate());
 	}
 
 	public function test_complex_1() {
@@ -76,11 +79,45 @@ class ExpressionTest extends PHPUnit_Framework_TestCase {
 
 	public function test_complex_3() {
 		// TODO: fill me
-		$this->assertFalse(true);
+		$node = new BinaryOp
+					( "*"
+					, new Number(2)
+					, new BinaryOp
+						( "-"
+						, new Number(3)
+						, new Number(4)
+						)
+					);
+		$this->assertEquals(-2, $node->evaluate());
 	}
 
 	public function test_complex_4() {
 		// TODO: fill me
-		$this->assertFalse(true);
+		$node = new BinaryOp
+					( "*"
+					, new BinaryOp
+						( "-"
+						, new Number(1)
+						, new Number(2)
+						)
+					, new BinaryOp
+						( "-"
+						, new Number(3)
+						, new Number(4)
+						)
+					);
+		$this->assertEquals(1, $node->evaluate());
+	}
+
+	public function test_invalid_number_construction()
+	{
+		try {
+
+			$NUM = new Number('a');
+			$this->assertFalse('has not thrown');
+		} catch( \InvalidArgumentException $e) {
+			$this->assertTrue(true);
+
+		}
 	}
 }
