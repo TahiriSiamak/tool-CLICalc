d5ea9befc6361b2d52408b084f37586d3aeb6edc
TahiriSiamak <tahirisiamak08@gmail.com>
2017-11-16 15:09:35 +0100

    Removed outdated comments

--- src/BinaryOp.php
+++ src/BinaryOp.php
@@ -1,57 +1,27 @@
 <?php
 namespace CaT\CLICalc;
 
-/**
- * A binary operation on two other expression is an expression as well.
- */
 class BinaryOp implements Expression {
-	/**
-	 * @var string
-	 */
+
 	private $op;
 
-	/**
-	 * @var Expression
-	 */
 	private $left;
 
-	/**
-	 * @var Expression
-	 */
 	private $right;
 
-	/**
-	 * @param	string		$op
-	 * @param	Expression	$left
-	 * @param	Expression	$right
-	 */
 	public function __construct(string $op, Expression $left, Expression $right) {
-		// TODO: fill me
 		$this->op = $op;
 		$this->left = $left;
 		$this->right = $right;
 	}
-
-	/**
-	 * @inheritdoc
-	 */
 	public function evaluate() {
-		// TODO: fill me
-		if($this->op == '+')
+		Switch($this->op)
 		{
-			return $this->left->evaluate() + $this->right->evaluate();
-		}
-		if($this->op == '-')
-		{
-			return $this->left->evaluate() - $this->right->evaluate();
-		}
-		if($this->op == '*')
-		{
-			return $this->left->evaluate() * $this->right->evaluate();
-		}
-		if($this->op == '/')
-		{
-			return $this->left->evaluate() / $this->right->evaluate();
+			case "+": return $this->left->evaluate() + $this->right->evaluate(); break;
+			case "-": return $this->left->evaluate() - $this->right->evaluate(); break;
+			case "*": return $this->left->evaluate() * $this->right->evaluate(); break;
+			case "/": return $this->left->evaluate() / $this->right->evaluate(); break;
+			default: echo "Kein gültiger Operator";
 		}
 	}
 }
